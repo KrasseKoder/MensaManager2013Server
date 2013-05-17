b3dfc8999f201674f52e7abb7b28faa931720558
danielp1000 <leinad.ffafp@gmail.com>
2013-05-17 17:50:18 +0200

    Started project

---
+++ src/main/java/com/github/krassekoder/Main.java
@@ -0,0 +1,11 @@
+package com.github.krassekoder;
+
+import com.trolltech.qt.core.QCoreApplication;
+
+public class Main{
+    public static void main(String[] args) {
+        QCoreApplication a = new QCoreApplication(args);
+
+        System.out.println("MensaManager2013 Server - Starting up");
+    }
+}
