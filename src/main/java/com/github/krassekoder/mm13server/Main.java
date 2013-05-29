5e0953ea82aded5ad04fdd01d7f207722e6efc9e
danielp1000 <leinad.ffafp@gmail.com>
2013-05-29 14:35:22 +0200

    Added application details

--- src/main/java/com/github/krassekoder/mm13server/Main.java
+++ src/main/java/com/github/krassekoder/mm13server/Main.java
@@ -5,6 +5,11 @@ import com.trolltech.qt.core.QCoreApplication;
 public class Main{
     public static void main(String[] args) {
         QCoreApplication a = new QCoreApplication(args);
+        
+        QCoreApplication.setApplicationName("MensaManager2013Server");
+        QCoreApplication.setApplicationVersion("Pre-Alpha");
+        QCoreApplication.setOrganizationName("KrasseKoder");
+        QCoreApplication.setOrganizationDomain("http://www.github.com/KrasseKoder/");
 
         System.out.println("MensaManager2013 Server - Starting up");
     }
