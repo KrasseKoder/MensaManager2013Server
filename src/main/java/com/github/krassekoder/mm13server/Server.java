d9b35b1bc1eede24d1323637eef1adcfd2f04efa
danielp1000 <leinad.ffafp@gmail.com>
2013-05-31 15:27:47 +0200

    Added default configuration

--- src/main/java/com/github/krassekoder/mm13server/Server.java
+++ src/main/java/com/github/krassekoder/mm13server/Server.java
@@ -2,9 +2,13 @@ package com.github.krassekoder.mm13server;
 
 import com.trolltech.qt.core.QCoreApplication;
 import com.trolltech.qt.core.QDir;
+import com.trolltech.qt.core.QFile;
 import com.trolltech.qt.core.QSettings;
+import com.trolltech.qt.gui.QApplication;
 import com.trolltech.qt.network.QHostAddress;
 import com.trolltech.qt.network.QTcpServer;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class Server extends QTcpServer {
 
@@ -16,7 +20,19 @@ public class Server extends QTcpServer {
     public void establishConnection() {
     }
 
-    public static void main(String[] args) {
+    public static String dir() throws URISyntaxException {
+        URI path = Server.class.getProtectionDomain().getCodeSource().getLocation().toURI();
+        String name = Server.class.getPackage().getName() + ".jar";
+        String path2 = path.getRawPath();
+        path2 = path2.substring(1);
+
+        if (path2.contains(".jar")) {
+            path2 = path2.replace(name, "");
+        }
+        return path2;
+    }
+
+    public static void main(String[] args) throws URISyntaxException {
         QCoreApplication a = new QCoreApplication(args);
 
         QCoreApplication.setApplicationName("MensaManager2013Server");
@@ -24,7 +40,29 @@ public class Server extends QTcpServer {
         QCoreApplication.setOrganizationName("KrasseKoder");
         QCoreApplication.setOrganizationDomain("http://www.github.com/KrasseKoder/");
 
-        QDir.setCurrent(QCoreApplication.applicationDirPath());
+        String jarPath = dir();
+        QDir.setCurrent(jarPath);
+        QSettings.setPath(QSettings.Format.IniFormat, QSettings.Scope.UserScope, jarPath);
+        
+        if (!QFile.exists("mm13server.ini")) {
+            System.out.println(QApplication.translate("Server", "Couldn't find configuration."));
+            QSettings settings = new QSettings("mm13server.ini", QSettings.Format.IniFormat);
+
+            settings.beginGroup("server");
+            settings.setValue("ip", "localhost");
+            settings.setValue("port", 0);
+            settings.endGroup();
+
+            settings.beginGroup("database");
+            settings.setValue("host", "db");
+            settings.setValue("driver", "SQLITE");
+            settings.setValue("user", "admin");
+            settings.setValue("password", "");
+            settings.endGroup();
+
+            settings.sync();
+            return;
+        }
 
         QSettings settings = new QSettings("mm13server.ini", QSettings.Format.IniFormat);
         Server s = new Server(new QHostAddress(settings.value("server-ip", "localhost").toString()),
