30f6530d766819a5c5731a646c8a9dca26822f71
danielp1000 <leinad.ffafp@gmail.com>
2013-06-17 19:15:02 +0200

    Moved settings evaluation to Main

--- src/main/java/com/github/krassekoder/mm13server/Main.java
+++ src/main/java/com/github/krassekoder/mm13server/Main.java
@@ -1,9 +1,9 @@
 package com.github.krassekoder.mm13server;
 
 import com.github.krassekoder.mm13server.network.Server;
+import com.trolltech.qt.QVariant;
 import com.trolltech.qt.core.QCoreApplication;
 import com.trolltech.qt.core.QDir;
-import com.trolltech.qt.core.QFile;
 import com.trolltech.qt.core.QSettings;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,58 +13,87 @@ import java.net.URISyntaxException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class Main {
 
     public static class InputThread extends Thread {
+
         @Override
         public void run() {
             BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-            while (true) {
+            while(true) {
                 try {
-                    if (r.readLine().toLowerCase().equals("stop")) {
+                    if(r.readLine().toLowerCase().equals("stop")) {
                         System.out.println(QCoreApplication.translate("Server", "Shutting down"));
                         QCoreApplication.quit();
                         return;
                     }
-                } catch (IOException ex) {
+                } catch(IOException ex) {
                     Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
         }
     }
 
-    private static String dir() throws URISyntaxException {
+    private static void preparePaths() throws URISyntaxException {
         URI path = Server.class.getProtectionDomain().getCodeSource().getLocation().toURI();
         String name = Server.class.getPackage().getName() + ".jar";
         String path2 = path.getRawPath();
         path2 = path2.substring(1);
 
-        if (path2.contains(".jar")) {
+        if(path2.contains(".jar")) {
             path2 = path2.replace(name, "");
         }
-        return path2;
+
+        QDir.setCurrent(path2);
+        QSettings.setPath(QSettings.Format.IniFormat, QSettings.Scope.UserScope, path2);
     }
 
-    private static void prepareSettings() {
-        if (!QFile.exists("mm13server.ini")) {
-            System.out.println(QCoreApplication.translate("Server", "Couldn't find configuration."));
-            QSettings settings = new QSettings("mm13server.ini", QSettings.Format.IniFormat);
+    private static QSettings prepareSettings() {
+        QSettings settings = new QSettings("mm13server.ini", QSettings.Format.IniFormat);
 
-            settings.beginGroup("server");
+        settings.beginGroup("server");
+        //string ip = "localhost"
+        settings.setValue("ip", settings.value("ip", "localhost"));
+        if(!QVariant.canConvertToString(settings.value("ip"))) {
             settings.setValue("ip", "localhost");
-            settings.setValue("port", 1996);
-            settings.endGroup();
-
-            settings.beginGroup("database");
-            settings.setValue("host", "mm13.db");
-            settings.setValue("driver", "SQLITE");
-            settings.setValue("user", "admin");
-            settings.setValue("password", "");
-            settings.endGroup();
-
-            settings.sync();
         }
+        //int port = 1996
+        settings.setValue("port", settings.value("port", 1996));
+        if(!QVariant.canConvertToInt(settings.value("port"))) {
+            settings.setValue("ip", 1996);
+        }
+        settings.endGroup();
+
+        settings.beginGroup("database");
+        //string driver = SQLITE
+        settings.setValue("driver", settings.value("driver", "SQLITE"));
+        if(!QVariant.canConvertToString(settings.value("driver"))) {
+            settings.setValue("driver", "SQLITE");
+        }
+        //string host =
+        settings.setValue("host", settings.value("host", ""));
+        if(!QVariant.canConvertToString(settings.value("host"))) {
+            settings.setValue("host", "");
+        }
+        //string name = mm13.db
+        settings.setValue("name", settings.value("name", "mm13.db"));
+        if(!QVariant.canConvertToString(settings.value("name"))) {
+            settings.setValue("name", "mm13.db");
+        }
+        //string user = admin
+        settings.setValue("user", settings.value("user", "admin"));
+        if(!QVariant.canConvertToString(settings.value("user"))) {
+            settings.setValue("user", "admin");
+        }
+        //string password =
+        settings.setValue("password", settings.value("password", ""));
+        if(!QVariant.canConvertToString(settings.value("password"))) {
+            settings.setValue("password", "");
+        }
+        settings.endGroup();
+
+        settings.sync();
+        return settings;
     }
 
     public static void main(String[] args) throws URISyntaxException {
@@ -75,14 +104,12 @@ public class Main {
         QCoreApplication.setOrganizationName("KrasseKoder");
         QCoreApplication.setOrganizationDomain("http://www.github.com/KrasseKoder/");
 
-        String jarPath = dir();
-        QDir.setCurrent(jarPath);
-        QSettings.setPath(QSettings.Format.IniFormat, QSettings.Scope.UserScope, jarPath);
+        preparePaths();
 
-        prepareSettings();
+        QSettings settings = prepareSettings();
 
-        QSettings settings = new QSettings("mm13server.ini", QSettings.Format.IniFormat);
         Server s = new Server(settings);
+        Database.init(settings);
 
         (new InputThread()).start();
 
--- src/main/java/com/github/krassekoder/mm13server/network/Server.java
+++ src/main/java/com/github/krassekoder/mm13server/network/Server.java
@@ -1,55 +1,28 @@
 package com.github.krassekoder.mm13server.network;
 
-import com.github.krassekoder.mm13server.network.Connection;
 import com.trolltech.qt.QVariant;
-import com.trolltech.qt.core.QCoreApplication;
-import com.trolltech.qt.core.QDir;
-import com.trolltech.qt.core.QFile;
 import com.trolltech.qt.core.QSettings;
 import com.trolltech.qt.network.QHostAddress;
 import com.trolltech.qt.network.QTcpServer;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-public class Server extends QTcpServer {
+public final class Server {
+    public static QTcpServer server;
     private ArrayList<Connection> connections;
 
-    public Server(QSettings settings) {
-        connections = new ArrayList<Connection>();
+    public Server(QSettings s) {
+        this.connections = new ArrayList<Connection>();
 
-        newConnection.connect(this, "establishConnection()");
-        settings.beginGroup("server");
-        listen(getAdress(settings), getPort(settings));
-        settings.endGroup();
-    }
+        server = new QTcpServer();
+        server.newConnection.connect(this, "establishConnection()");
+        s.beginGroup("server");
+        server.listen(new QHostAddress(QVariant.toString(s.value("address"))),
+               QVariant.toInt(s.value("port")));
 
-    private QHostAddress getAdress(QSettings s) {
-        Object address = s.value("ip");
-        if(!QVariant.canConvertToString(address)) {
-            System.out.println(String.format(tr("Invalid value for %1$s, should be %2$s!"), "server/ip", "String"));
-            System.out.println(String.format(tr("Using default value for %1$s: %2$s!"), "server/ip", "localhost"));
-            return new QHostAddress(QHostAddress.SpecialAddress.LocalHost);
-        }
-        return new QHostAddress(QVariant.toString(address));
-    }
-
-    private int getPort(QSettings s) {
-        Object port = s.value("port");
-        if (!QVariant.canConvertToInt(port)){
-            System.out.println(String.format(tr("Invalid value for %1$s, should be %2$s!"), "server/port", "int"));
-            System.out.println(String.format(tr("Using default value for %1$s: %2$s!"), "server/port", "1996"));
-            return 1996;
-        }
-        return QVariant.toInt(port);
+        s.endGroup();
     }
 
     public void establishConnection() {
-        connections.add(new Connection(nextPendingConnection()));
+        connections.add(new Connection(server.nextPendingConnection()));
     }
 }
